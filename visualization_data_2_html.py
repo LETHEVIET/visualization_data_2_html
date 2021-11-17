4c42fd2589a0e6ad123cdaf8a7203a519a06499f
LETHEVIET <50667900+LETHEVIET@users.noreply.github.com>
2021-11-17 12:40:21 +0700

    Add files via upload

---
+++ visualization_data_2_html.py
@@ -0,0 +1,102 @@
+import argparse
+from pathlib import Path
+from PIL import Image
+import base64
+from io import BytesIO
+import sys
+from tqdm import tqdm
+
+class visualization_data_2_html:
+  def __init__(self, images_dir, labels_dir, name, save_dir, labels):
+    self.imgDir = images_dir
+    self.labDir = labels_dir
+    self.name = name
+    self.save_dir = save_dir
+    self.labels = labels
+  
+  def run(self):
+    imgDir = self.imgDir
+    labDir = self.labDir
+    labels = self.labels
+    ls_img = list(Path(imgDir).glob('**/*.jpg'))
+    ls_lab = list(Path(labDir).glob('**/*.txt'))
+    ls_img = sorted(ls_img)
+    ls_lab = sorted(ls_lab)
+
+    no_mask = []
+    mask = []
+    incorrect_mask = []
+
+    category = [[]]
+    for i in range(len(labels)):
+      category.append([])
+
+    for i in tqdm(range(0, len(ls_img))):
+
+      imgdir = str(ls_img[i])
+      labdir = str(ls_lab[i])
+
+      f = open(labdir, "r")
+      image = Image.open(imgdir)
+      width, height = image.size
+
+      for x in f:
+        bouding_box_info = list(map(float, x.split(" ")))
+        
+        #print(bouding_box_info)
+        x_min = (bouding_box_info[1] - bouding_box_info[3]/2) * width
+        y_min = (bouding_box_info[2] - bouding_box_info[4]/2) * height
+        x_max = (bouding_box_info[1] + bouding_box_info[3]/2) * width
+        y_max = (bouding_box_info[2] + bouding_box_info[4]/2) * height
+
+        bouding_box = (x_min, y_min, x_max, y_max)
+        cropImg = image.crop(bouding_box)
+        
+        buffered = BytesIO()
+        cropImg.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue())
+        img_base64 = bytes("data:image/jpeg;base64,", encoding='utf-8') + img_str
+        #print(img_base64.decode("utf-8"))
+        category[int(bouding_box_info[0])].append((ls_img[i].stem, img_base64.decode("utf-8")))
+
+    print("\t")
+    for i in range(len(labels)):
+      print(labels[i], "\t" , len(category[i]))
+      message = '<html><head></head><style>.page { max-width: 800px ; margin: auto; } img {filter: drop-shadow(0 0 0.75rem ); height: 182px ; padding-left: 28px ; padding-top: 9px ;}</style><body><div class="page"><div>'
+
+      fileName = ""
+      for j in range(len(category[i])):
+        filename, base64Str = category[i][j]
+        if filename != fileName:
+          message = message + '<h3>'+ filename + '</h3> <br>'
+          fileName = filename
+        message = message + '<img src="' + base64Str + '" alt="' + filename + '" />'
+
+      message = message + '</div></div></body></html>'
+
+      saveDir = Path(self.save_dir)
+      fileName = self.name + "_" + labels[i] + ".html"
+      saveDir = saveDir / fileName
+      f = open( saveDir ,'w')
+      f.write(message)
+      f.close()
+    
+
+def parser():
+    args = argparse.ArgumentParser()
+    args.add_argument('--images-dir', type=str, help='specify your images path', required=True)
+    args.add_argument('--labels-dir', type=str, help='specify your labels path', required=True)
+    args.add_argument('--save-dir', type=str, help='specify your save path', required=True)
+    args.add_argument('--name', type=str, help='specify your html file name', required=True)
+    args.add_argument('--labels', nargs="+", help='list of your bounding box labels', required=True)
+    args = args.parse_args()
+    
+    return args
+
+def main(opt):
+  #print(vars(opt))
+  html = visualization_data_2_html(**vars(opt))
+  html.run()
+
+if __name__ == "__main__":
+    main(parser())
